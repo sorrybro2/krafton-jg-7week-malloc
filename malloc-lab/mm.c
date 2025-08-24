393dc518f7e21011f587698a9890bb1f4f0f64d4
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-08-24 15:05:16 +0900

    coalesce

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -52,6 +52,20 @@ team_t team = {
 
 #define PACK(size, alloc) ((unsigned)((size)|(alloc))) // 각 데이터 블럭 당 헤더
 #define PUT(p, val) (*(unsigned*)(p) = (val)) // 주소값에 해당 데이터 블럭 넣기
+#define GET(p) (*(unsigned*)(p)) // 주소 읽기
+
+#define GET_SIZE(p) (GET(p) & ~0x7) // header와 footer에서 size 추출
+#define GET_ALLOC(p) (GET(p) & 0x1) // header와 footer에서 alloc 추출
+
+#define HDRP(bp) ((char *)(bp) - WSIZE) // 블록 포인터로부터 header 위치 계산
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) // 블록 포인터로부터 footer 위치 계산
+
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE)) // 다음 블록의 payload 시작 주소
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char))) // 현재 블록 바로 앞에 있는 블록의 payload 시작 주소
+
+#define ALIGN(size) (((size) + (DSIZE-1)) & ~0x7) // 항상 8의 배수로 맞춤
+#define MAX(x, y) ((x) > (y) ? (x) : (y)) // 둘이 비교해서 큰거
+#define MIN_BLOCK (2*DSIZE) // (header+footer)(8) + 최소 payload(8) = 16B 
 
 static char *heap_listp = NULL; // 실질적 주소는 프롤로그 블록이므로 
 
@@ -63,7 +77,7 @@ int mm_init(void)
     PUT(heap_listp + (3 * WSIZE), PACK(0,1)); // 프롤로그 헤더 (0/alloc)
     heap_listp += (2 * WSIZE); // 페이로드 시작 지점
 
-    if(extend_heap(heap_listp / WSIZE) ==  NULL){
+    if(extend_heap(heap_listp / WSIZE) ==  NULL){ // 확장되는 힙이 8배수 정렬 유지가 안됐다면
         return -1;
     }
 
@@ -72,11 +86,55 @@ int mm_init(void)
 
 static void *extend_heap(size_t words) // 힙을 워드만큼 확장하여 가용블록으로 초기화
 {
-    char *bp; // 
-    size_t size;
+    char *bp; // 새로 확장된 블록의 시작주소를 가리키는 포인터 생성
+    size_t size; // 실제로 저장되는 바이트 사이즈
 
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE; // 정렬 유지 8배수를 유지하기 위해 
 
+    if ((bp = mem_sbrk(size)) == (void *)-1){ // 힙 확장 : 실패 시 (void *)-1 -> NULL 반환
+        return NULL;
+    }
+
+    // 새로 생긴 가용 블록의 헤더 풋터 가용 데이터이므로 alloc = 0
+    PUT(HDRP(bp), PACK(size, 0)); // new free header
+    PUT(FTRP(bp), PACK(size, 0)); // new free footer
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(size, 0)) // new epligue
+
+    // 인접 free와 병합
+    return coalesce(bp);
 }
+
+// 인접 free와 새로 추가된 가용 블록과 병합
+static void coalesce(void *bp)
+{
+    // 이전 블록의 alloc 할당을 탐색하므로 PREV_BLKP 이전 블록의 payload에 위치한 FDRP 푸터를 검색
+    unsigned prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    // 다음 블록의 alloc 할당을 탐색하므로 NEXT_BLKP 다음 블록의 payload에 위치한 HDRP 헤더를 검색
+    unsigned next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    // 헤더에 블록 전체 크기 + alloc 비트가 저장 되어있으므로 size만 추출해서 사용
+    size_t size = GET_SIZE(HDRP(bp));
+
+    // free 가용 블럭인 경우에만 병합 과정이 이뤄져야한다! (경계 태그 병합 4가지 경우)
+    if (prev_alloc && next_alloc){ // 1. 이전과 다음 블록이 할당되어 있을 때 -> 병합 x
+        return bp; // 
+    }else if (prev_alloc && !next_alloc){ // 2. 이전 블록은 할당, 다음 블록은 가용 -> 뒤와 병합
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))); // 현재 블록 크기(size)에 뒤에 블록 크기 추가
+        PUT(HDRP(bp), PACK(size, 0)); // 헤더 최신화 (현재 위치 유지)
+        PUT(FTRP(bp), PACK(size, 0)); // 푸터 최신화 (뒤 블록 위치) -> 뒤와 병합했기 때문!
+    }else if (!prev_alloc && next_alloc){ // 3. 다음 블록은 할당, 이전 블록이 가용 -> 앞과 병합
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))); // 현재 블록 크기에 앞에 블록 크기 추가
+        PUT(FTRP(bp), PACK(size, 0)); // 푸터 최신화 (현재 위치 유지)
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0)); // 헤더 최신화 (앞 블록 위치)
+        bp = PREV_BLKP(bp); // 앞에 붙였으므로 시작점 포인터를 앞에 블록으로 바꿈
+    }else{ // 4. 이전 다음 블록 모두 가용 -> 모두 병합
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(HDRP(PREV_BLKP(bp))); // 현재 블록 크기에 앞+뒤 블록 크기 추가
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0)); // 헤더 최신화 (앞 블록 위치)
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0)); // 푸터 최신화 (뒤 블록 위치)
+        bp = PREV_BLKP(bp); // 시작 포인터 전 블록으로 바꿈
+    }
+    return bp; //합병 완!
+}
+
 
 /*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
